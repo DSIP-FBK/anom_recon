7771e3f63ef7f8320fac6f9669b0bb4c66a84752
Camilletti <camilletti92@gmail.com>
2025-08-19 14:11:28 +0200

    model's checkpoints paths

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -9,22 +9,40 @@ source ".venv/bin/activate"
 # -----------------
 
 # train with 7 wr
-python src/train.py -m experiment=europe_t2m_ERA5_7wr data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_t2m_ERA5_7wr \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/7wr \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0
 
 # train with 4 wr in winter
-python src/train.py -m experiment=europe_t2m_ERA5_4wr_winter data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_t2m_ERA5_4wr_winter \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/4wr_winter \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0
 
 # train with 4 wr in summer
-python src/train.py -m experiment=europe_t2m_ERA5_4wr_summer data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_t2m_ERA5_4wr_summer \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/4wr_summer \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0
 
 # train with NAO index in winter
-python src/train.py -m experiment=europe_t2m_ERA5_NAO_winter data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_t2m_ERA5_NAO_winter \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/NAO_winter \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0
 
 # train with NAO index in summer
-python src/train.py -m experiment=europe_t2m_ERA5_NAO_summer data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_t2m_ERA5_NAO_summer \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/NAO_summer \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0
 
 # train with 0 wr
-python src/train.py -m experiment=europe_t2m_ERA5_0wr data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_t2m_ERA5_0wr \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/0wr \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0 
 
 
 # -----------------
@@ -32,19 +50,37 @@ python src/train.py -m experiment=europe_t2m_ERA5_0wr data.num_workers=0 seed=0,
 # -----------------
 
 # train with 7 wr
-python src/train.py -m experiment=europe_tp_ERA5_7wr data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_tp_ERA5_7wr \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/7wr \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0
 
 # train with 4 wr in winter
-python src/train.py -m experiment=europe_tp_ERA5_4wr_winter data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_tp_ERA5_4wr_winter \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/4wr_winter \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0 
 
 # train with 4 wr in summer
-python src/train.py -m experiment=europe_tp_ERA5_4wr_summer data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_tp_ERA5_4wr_summer \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/4wr_summer \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0
 
 # train with NAO index in winter
-python src/train.py -m experiment=europe_tp_ERA5_NAO_winter data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_tp_ERA5_NAO_winter \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/NAO_winter \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0
 
 # train with NAO index in summer
-python src/train.py -m experiment=europe_tp_ERA5_NAO_summer data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_tp_ERA5_NAO_summer \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/NAO_summer \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0 
 
 # train with 0 wr
-python src/train.py -m experiment=europe_tp_ERA5_0wr data.num_workers=0 seed=0,3,67,72,12,111
+python src/train.py -m experiment=europe_tp_ERA5_0wr \
+    hydra.sweep.dir=${paths.log_dir}/${task_name}/0wr \
+    seed=0,3,67,72,12,111 \
+    data.num_workers=0 
